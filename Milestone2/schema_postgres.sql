78c3601cee1b4e3c9e4fbc393c8b7c6bd4286120
calvinchenwisc <90725147+calvinchenwisc@users.noreply.github.com>
2021-12-22 21:58:22 -0600

    Delete Milestone2 directory

--- Milestone2/schema_postgres.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE tweets;
-
-CREATE TABLE tweets (
-   time_stamp		TIMESTAMP		NOT NULL,  
-   time_group		TIMESTAMP		NOT NULL,   
-   word				varchar(1000)	NOT NULL,
-   word_count		INT				NOT NULL);
